9aad6770201568895f5295ad78af94cf083da4f9
刘吟啸 <yinxiao.liu@rokid.com>
2018-07-09 12:02:28 +0800

    [update] add some simple unit test

---
+++ unit_test.go
@@ -0,0 +1,48 @@
+package blockchain
+
+import (
+	"crypto/sha256"
+	"testing"
+)
+
+func TestBlockChainCreation(t *testing.T) {
+	blockChain := BlockChain{}
+	if blockChain.Current == nil {
+		blockChain.Creation()
+	}
+
+	if blockChain.Current != nil {
+		t.Log(blockChain.Current)
+	} else {
+		t.Error("creation failed")
+	}
+}
+
+func TestBlockChainGenerate(t *testing.T) {
+	nodeArray := [8]HashCode{
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000000")),
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000111")),
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000222")),
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000333")),
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000444")),
+		sha256.Sum256([]byte("0000000000000000000000000000000000000000000000000000000000000555")),
+		sha256.Sum256([]byte("000000000000000000000000000000000000000000000000000000000000066666")),
+		sha256.Sum256([]byte("000000000000000000000000000000000000000000000000000000000000077")),
+	}
+
+	blockChain := BlockChain{}
+	if blockChain.Current == nil {
+		blockChain.Creation()
+	}
+
+	block, err := blockChain.GenerateBlock(nodeArray[:], 20)
+	if err != nil {
+		t.Error(err)
+	} else {
+		if blockChain.Current.Header.Version != 0 {
+			blockChain.Current.NextHash = block.Hash
+			blockChain.Current = block
+		}
+		t.Log(blockChain.Current)
+	}
+}
